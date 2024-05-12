2c318764bab79213e20570ea2b0e17d7b4be5fc3
SzymonSkoczylas <ss300476@student.polsl.pl>
2024-05-12 16:15:03 +0200

    zmiany essa

--- BeaconBox/ViewController.swift
+++ BeaconBox/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import EstimoteUWB
 
 class ViewController: UIViewController {
-
+    
+    let uwb = EstimoteUWBManagerExample()
+    
+    @IBOutlet weak var text1: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func didDiscover(device: UWBIdentifiable, with rssi: NSNumber, from manager: EstimoteUWBManager)
+    {
+        var tmp = "Discovered device: \(device.publicIdentifier) rssi: \(rssi)"
+        print(tmp)
+        text1.text = tmp
+    }
 }
 
+class EstimoteUWBManagerExample: NSObject, ObservableObject {
+    private var uwbManager: EstimoteUWBManager?
+
+    override init() {
+        super.init()
+        setupUWB()
+    }
+
+    private func setupUWB() {
+        uwbManager = EstimoteUWBManager(delegate: self,
+                                        options: EstimoteUWBOptions(shouldHandleConnectivity: true,
+                                                                    isCameraAssisted: false))
+        uwbManager?.startScanning()
+    }
+}
+
+
+// REQUIRED PROTOCOL
+extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
+    func didUpdatePosition(for device: EstimoteUWBDevice) {
+        print("Position updated for device: \(device)")
+    }
+    
+    // OPTIONAL
+    func didDiscover(device: UWBIdentifiable, with rssi: NSNumber, from manager: EstimoteUWBManager) {
+        print("Discovered device: \(device.publicIdentifier) rssi: \(rssi)")
+        // if shouldHandleConnectivity is set to true - then you could call manager.connect(to: device)
+        // additionally you can globally call discoonect from the scope where you have inititated EstimoteUWBManager -> disconnect(from: device) or disconnect(from: publicId)
+    }
+    
+    // OPTIONAL
+    func didConnect(to device: UWBIdentifiable) {
+        print("Successfully connected to: \(device.publicIdentifier)")
+    }
+    
+    // OPTIONAL
+    func didDisconnect(from device: UWBIdentifiable, error: Error?) {
+        print("Disconnected from device: \(device.publicIdentifier)- error: \(String(describing: error))")
+    }
+    
+    // OPTIONAL
+    func didFailToConnect(to device: UWBIdentifiable, error: Error?) {
+        print("Failed to conenct to: \(device.publicIdentifier) - error: \(String(describing: error))")
+    }
+
+    // OPTIONAL PROTOCOL FOR BEACON BLE RANGING
+//    func didRange(for beacon: EstimoteBLEDevice) {
+//        print("Beacon did range: \(beacon)")
+//    }
+}
