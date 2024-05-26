7d310cb8fc5bdccba53518675515f5e319bd9e3b
SzymonSkoczylas <ss300476@student.polsl.pl>
2024-05-26 16:37:27 +0200

    Autoryzacja in range 5m

--- BeaconBox/ViewController.swift
+++ BeaconBox/ViewController.swift
@@ -16,8 +16,8 @@ import SwiftUI
         //-----------------------------//
 
 var trustedBeaconIDs = [String]()       // tu powinny sie znalesc id beaconow ktore moga przeprowadzic autoryzacje
-var discoveredBeaconIDs = [String]()
-
+var beaconsInRange = [(ID : String, distance : Float, inAuthRange : Bool)]()
+var isAuthorizedByBeacon : Bool = false
         //-----------------------------//
         //-----------------------------//
         //        Controller code      //
@@ -35,22 +35,7 @@ class ViewController: UIViewController {
     @IBAction func AuthorizeByBeaconButtonClicked(_ sender: UIButton) {
         
         //sprawdz czy to dziala
-        for id in trustedBeaconIDs
-            { print("Trusted Beacon ID : " + id) }
-        for id in discoveredBeaconIDs
-            { print("Discovered Beacon ID : " + id) }
-        print("clicked")
-        
-        //jezeli tak to to powinno swapnac do view controllera 2
-
-        var isAuthorizedByBeacon : Bool = false
-        for trustedBeaconID in trustedBeaconIDs {
-            for discoveredBeaconID in discoveredBeaconIDs {
-                if discoveredBeaconID == trustedBeaconID{
-                    isAuthorizedByBeacon = true
-                }
-            }
-        }
+       
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let VC2 = storyboard.instantiateViewController(identifier: "ViewController2")
@@ -63,6 +48,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 }
 
 
@@ -93,6 +79,21 @@ class EstimoteUWBManagerExample: NSObject, ObservableObject {
 extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
     func didUpdatePosition(for device: EstimoteUWBDevice) {
         //print("Position updated for device: \(device)")
+        for (index, _) in beaconsInRange.enumerated() {
+            
+            if (beaconsInRange[index].ID == device.publicIdentifier){
+                beaconsInRange[index].distance = device.distance
+            }
+            
+            if(beaconsInRange[index].distance < 5.0){
+                beaconsInRange[index].inAuthRange = true
+            }
+            else{
+                beaconsInRange[index].inAuthRange = false
+            }
+            
+        }
+        Authorization()
     }
     
     // OPTIONAL
@@ -102,8 +103,7 @@ extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
         // additionally you can globally call discoonect from the scope where you have inititated EstimoteUWBManager -> disconnect(from: device) or disconnect(from: publicId)
         
         trustedBeaconIDs.append(device.publicIdentifier)
-        discoveredBeaconIDs.append(device.publicIdentifier)
-        
+        beaconsInRange.append((device.publicIdentifier,0.0,false))
     }
     
     // OPTIONAL
@@ -120,7 +120,30 @@ extension EstimoteUWBManagerExample: EstimoteUWBManagerDelegate {
     func didFailToConnect(to device: UWBIdentifiable, error: Error?) {
         print("Failed to conenct to: \(device.publicIdentifier) - error: \(String(describing: error))")
     }
-
+    
+    func Authorization()
+    {
+        for id in trustedBeaconIDs
+        { print("Trusted Beacon ID : " + id) }
+        for beacon in beaconsInRange
+        { print("Discovered Beacon ID : " + beacon.ID) }
+        print("clicked")
+        
+        //jezeli tak to to powinno swapnac do view controllera 2
+        
+        
+        
+        for trustedBeaconID in trustedBeaconIDs {
+            for discoveredBeacon in beaconsInRange {
+                if discoveredBeacon.ID == trustedBeaconID && discoveredBeacon.inAuthRange == true {
+                    isAuthorizedByBeacon = true
+                }
+                else{
+                    isAuthorizedByBeacon = false
+                }
+            }
+        }
+    }
     // OPTIONAL PROTOCOL FOR BEACON BLE RANGING
 //    func didRange(for beacon: EstimoteBLEDevice) {
 //        print("Beacon did range: \(beacon)")
